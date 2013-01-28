269de2843e17ec7d382c3f74c218d2729dfe138d
Ignacio Huerta <ignacio@ihuerta.net>
2013-01-28 13:21:26 +0100

    Handle FB errors in post iteration

--- copiedbook.rb
+++ copiedbook.rb
@@ -19,9 +19,6 @@ CONSTANT = "..."
 variable = "..."
 
 
-
-
-
 # Our requires
 Dir.glob(current_path + '/lib/*.rb') {|file| require file}
 
@@ -49,25 +46,50 @@ class MyClass
     puts "Not enough arguments. Example: \nruby copiedbook.rb 270335753072803 AAACEdEose0cBAIGTvuh38rLhsDD4jJdJjApGjH8z6uUmZCMH0I0IptiQ3z9qbbzVNITJtLJ2ReL4ZBwJGZCAtHAdpMT1wYlY1Qu9A71rnwxf8RPG3QZA my_file.json"
   end
 
+  def get_feed
+    if @token.empty?
+      puts "Introduce a new token"
+      @token = gets.chomp
+    end    
+    @graph = Koala::Facebook::API.new(@token)
+    begin
+      feed = @graph.get_connections(@fanpage_id, "feed")
+    rescue Exception => ex
+      @token = ""
+      puts "Facebook has returned an error: #{ex.message}"
+      feed = get_feed
+    end
+  end
+
+  def get_nextpage(feed,parameters = nil)
+    begin
+      parameters = feed.next_page_params if parameters == nil 
+      return nil if parameters == nil
+      feed = @graph.get_page(parameters)
+    rescue Exception => ex
+      puts "Facebook has returned an error: #{ex.message}"
+      @token = ""
+      feed = get_feed
+      get_nextpage(feed, parameters)
+    end
+  end
+
   def main
     puts(" ---- STARTING ----\n\n")
-
-
-
     
     # Connect to Facebook and get the feed with all posts
     require 'koala'
     @graph = Koala::Facebook::API.new(@token)
     output = []
-    #### TODO: rescue from errors
-    feed = @graph.get_connections(@fanpage_id, "feed")
+
+    feed = get_feed
+
     counter = 1
     until feed == nil
       output += feed
       puts "We are in page #{counter} of the fanpage's feed"
       counter += 1
-      #### TODO: rescue from errors
-      feed = feed.next_page
+      feed = get_nextpage(feed)
     end
     
     
@@ -76,14 +98,13 @@ class MyClass
       if post["comments"]["count"] > 0
         puts "  The post #{post["id"]} has #{post["comments"]["count"]} comments. We are going to retrieve them now."
         comments = []
-        #### TODO: rescue from errors
         comment_feed = @graph.get_connections(post["id"], "comments")
         until comment_feed == nil
           comments += comment_feed
-          #### TODO: rescue from errors
           comment_feed = comment_feed.next_page
         end
         post["comments"] = comments
+        #puts "We have retrieved #{post["comments"].size} comments"
         output[index] = post
       end
     end
@@ -98,6 +119,7 @@ class MyClass
 
   def run_it
     if ARGV.size == 3
+      ARGV.clear
       main
     else
       case ARGV[0]
