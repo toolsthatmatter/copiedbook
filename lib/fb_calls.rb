581abeee948b1f8ddedc31f9ff40a896a9575804
Markus Kuhnt <markus.kuhnt@tridoco.com>
2013-01-28 16:12:17 +0100

    add require 'json'

--- lib/fb_calls.rb
+++ lib/fb_calls.rb
@@ -1,10 +1,12 @@
+require 'json'
+
 module FBCalls
 
   def get_feed
     if @token.empty?
       puts "Introduce a new token"
       @token = gets.chomp
-    end    
+    end
     @graph = Koala::Facebook::API.new(@token)
     begin
       feed = @graph.get_connections(@fanpage_id, "feed")
@@ -15,7 +17,7 @@ module FBCalls
 
   def get_next_page(feed,parameters = nil)
     begin
-      parameters = feed.next_page_params if parameters == nil 
+      parameters = feed.next_page_params if parameters == nil
       return nil if parameters == nil
       feed = @graph.get_page(parameters)
     rescue Exception => ex
@@ -23,7 +25,7 @@ module FBCalls
       get_next_page(feed, parameters)
     end
   end
-  
+
   def get_comment_feed(post)
     begin
       comment_feed = @graph.get_connections(post["id"], "comments")
@@ -32,12 +34,12 @@ module FBCalls
       get_comment_feed
     end
   end
-  
+
   def handle_error(ex)
     File.open(@output_file, 'w') {|f| f.write(@output.to_json) }
     puts "Facebook has returned an error: #{ex.message}"
     @token = ""
     return get_feed
   end
-  
+
 end
