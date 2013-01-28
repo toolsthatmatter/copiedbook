5ae92d17a211907ed02d49858609cb1992238a75
Ignacio Huerta <ignacio@ihuerta.net>
2013-01-28 13:42:22 +0100

    Handle errors in comments iteration

--- copiedbook.rb
+++ copiedbook.rb
@@ -24,6 +24,7 @@ Dir.glob(current_path + '/lib/*.rb') {|file| require file}
 
 class MyClass
   include Helpers
+  include FBCalls
 
   def initialize(log_file)
     @log_file = log_file
@@ -46,71 +47,42 @@ class MyClass
     puts "Not enough arguments. Example: \nruby copiedbook.rb 270335753072803 AAACEdEose0cBAIGTvuh38rLhsDD4jJdJjApGjH8z6uUmZCMH0I0IptiQ3z9qbbzVNITJtLJ2ReL4ZBwJGZCAtHAdpMT1wYlY1Qu9A71rnwxf8RPG3QZA my_file.json"
   end
 
-  def get_feed
-    if @token.empty?
-      puts "Introduce a new token"
-      @token = gets.chomp
-    end    
-    @graph = Koala::Facebook::API.new(@token)
-    begin
-      feed = @graph.get_connections(@fanpage_id, "feed")
-    rescue Exception => ex
-      @token = ""
-      puts "Facebook has returned an error: #{ex.message}"
-      feed = get_feed
-    end
-  end
-
-  def get_nextpage(feed,parameters = nil)
-    begin
-      parameters = feed.next_page_params if parameters == nil 
-      return nil if parameters == nil
-      feed = @graph.get_page(parameters)
-    rescue Exception => ex
-      puts "Facebook has returned an error: #{ex.message}"
-      @token = ""
-      feed = get_feed
-      get_nextpage(feed, parameters)
-    end
-  end
-
   def main
     puts(" ---- STARTING ----\n\n")
     
     # Connect to Facebook and get the feed with all posts
     require 'koala'
     @graph = Koala::Facebook::API.new(@token)
-    output = []
+    @output = []
 
     feed = get_feed
 
     counter = 1
     until feed == nil
-      output += feed
+      @output += feed
       puts "We are in page #{counter} of the fanpage's feed"
       counter += 1
-      feed = get_nextpage(feed)
+      feed = get_next_page(feed)
     end
     
     
     # Iterate through the feed to get all comments
-    output.each_with_index do |post, index|
+    @output.each_with_index do |post, index|
       if post["comments"]["count"] > 0
         puts "  The post #{post["id"]} has #{post["comments"]["count"]} comments. We are going to retrieve them now."
         comments = []
-        comment_feed = @graph.get_connections(post["id"], "comments")
+        comment_feed = get_comment_feed(post)
         until comment_feed == nil
           comments += comment_feed
-          comment_feed = comment_feed.next_page
+          comment_feed = get_next_page(comment_feed)
         end
         post["comments"] = comments
-        #puts "We have retrieved #{post["comments"].size} comments"
-        output[index] = post
+        @output[index] = post
       end
     end
 
     # Write the file
-    File.open(@output_file, 'w') {|f| f.write(output.to_json) }
+    File.open(@output_file, 'w') {|f| f.write(@output.to_json) }
 
     puts("\n\n ---- ENDING ----\n")
     clean_the_log 
